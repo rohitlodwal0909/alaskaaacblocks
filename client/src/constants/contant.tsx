65adfbc6075352f1ec6bdf90db7e16f2aee9075b
rohitlodwal0909 <shardalodwal@gmail.com>
2025-07-08 18:37:41 +0530

    some module changes db and backend

--- client/src/constants/contant.tsx
+++ client/src/constants/contant.tsx
@@ -1,3 +1,3 @@
-export const apiUrl = "http://147.93.96.13:5000/api";
-// export const apiUrl = "http://localhost:5000/api";
+// export const apiUrl = "http://147.93.96.13:5000/api";
+export const apiUrl = "http://localhost:5000/api";
  export const fakeUrl = "https://fake-json-api.mock.beeceptor.com/users";
